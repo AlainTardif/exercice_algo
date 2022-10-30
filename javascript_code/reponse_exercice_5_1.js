95c30827665a639420e810501d0595c7a4b17208
Alain Tardif <tardif.alain@gmail.com>
2022-10-30 14:12:08 +0100

    validation exercice 5.1

--- javascript_code/reponse_exercice_5_1.js
+++ javascript_code/reponse_exercice_5_1.js
@@ -1,16 +1,11 @@
 // Equivalent Algo (exercice_5_1) en Javascript
-/*** Variable N en Entier
 
-Debut
-    N <- 0
-    Ecrire "Entrez un nombre entre 1 et 3"
-    Tant Que N < 1 ou N > 3
-        Lire N
-            Si N < 1 ou N > 3 Alors
-                Ecrire "Saisie erronée. Recommencez"
-            Fin Si
-    Fin Tant Que
-Fin 
-***/
+var N = 0;
 
+prompt("Entrez un nombre entre 1 et 3") 
+    while ((N < 1) || (N > 3)){
+        window.alert("Saisie incorrecte, veuillez recommencer")
+        N = prompt("Entrez un nombre entre 1 et 3") 
+}
+alert("C'est parfait")
 
