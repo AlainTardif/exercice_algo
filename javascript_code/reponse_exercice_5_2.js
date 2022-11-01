a1cf5306a2b4f3a9c63a101c3937e5a70129fd09
Alain Tardif <tardif.alain@gmail.com>
2022-11-01 15:49:16 +0100

    validation exercice 5.2

--- javascript_code/reponse_exercice_5_2.js
+++ javascript_code/reponse_exercice_5_2.js
@@ -1,1 +1,14 @@
 // Equivalent Algo (exercice_5_2) en Javascript
+
+var nB = 0;
+
+prompt("Ecrire un nombre entre 10 et 20")
+  while(nB < 10) {
+    alert('Plus grand')
+    nB = prompt("Ecrire un nombre entre 10 et 20")
+  }
+  while(nB > 20) {
+    alert('Plus petit')
+    nB = prompt("Ecrire un nombre entre 10 et 20")
+  }
+  alert("C'est parfait")
