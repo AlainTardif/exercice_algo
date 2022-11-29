0cb7a7ff42e1470e24c8d8e5aa169bb9bd78970c
Alain Tardif <tardif.alain@gmail.com>
2022-11-29 18:15:30 +0100

    mise à jour

--- javascript_code/reponse_exercice_5_3.js
+++ javascript_code/reponse_exercice_5_3.js
@@ -1,1 +1,15 @@
 // Equivalent Algo (exercice_5_3) en Javascript
+
+var iCompteur = 0;
+var iNbr = 0; 
+var iResult = "";
+
+
+prompt("Ecrire un nombre")
+iCompteur = 0;
+while (iCompteur < 10) {
+  iCompteur++;
+  iNbr++;
+  iResult += iCompteur + " ";
+}
+alert(iResult);
